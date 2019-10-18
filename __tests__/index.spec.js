63a2b6d212c35c1c63db73fc3c2d1e96b78b9589
Martin Jantosovic <jantosovic.martin@gmail.com>
2019-10-18 11:13:05 +0200

    style: Format files

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -8,109 +8,108 @@ const webpack = require('webpack');
 const InlineCSSModulesNamesPlugin = require('..');
 
 describe('webpack-inline-cssmodules-names-plugin', () => {
-	const fixturesDirectory = path.join(__dirname, 'fixtures');
-	const buildDirectory = path.join(__dirname, 'build');
+    const fixturesDirectory = path.join(__dirname, 'fixtures');
+    const buildDirectory = path.join(__dirname, 'build');
 
-	beforeAll(() => {
-		rimraf.sync(buildDirectory);
-	});
+    beforeAll(() => {
+        rimraf.sync(buildDirectory);
+    });
 
-	afterAll(() => {
-		rimraf.sync(buildDirectory);
-	});
+    afterAll(() => {
+        rimraf.sync(buildDirectory);
+    });
 
-	test('should remove constant classname keys', done => {
-		const inputDirectory = path.join(fixturesDirectory, 'basic');
-		const outputDirectory = path.join(buildDirectory, 'basic');
-		const config = {
-			context: inputDirectory,
-			entry: './index.js',
-			mode: 'production',
-			target: 'node',
-			module: {
-				rules: [
-					{
-						test: /\.css/,
-						use: [{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								onlyLocals: true,
-							},
-						}]
-					}
-				]
-			},
-			optimization: {
-				runtimeChunk: false,
-				moduleIds: 'named',
-				chunkIds: 'named',
-				minimize: false,
-			},
-			output: {
-				path: outputDirectory,
-			},
-			plugins: [
-				new InlineCSSModulesNamesPlugin([/\.css$/]),
-			],
-		};
+    test('should remove constant classname keys', (done) => {
+        const inputDirectory = path.join(fixturesDirectory, 'basic');
+        const outputDirectory = path.join(buildDirectory, 'basic');
+        const config = {
+            context: inputDirectory,
+            entry: './index.js',
+            mode: 'production',
+            target: 'node',
+            module: {
+                rules: [
+                    {
+                        test: /\.css/,
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: true,
+                                    onlyLocals: true,
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+            optimization: {
+                runtimeChunk: false,
+                moduleIds: 'named',
+                chunkIds: 'named',
+                minimize: false,
+            },
+            output: {
+                path: outputDirectory,
+            },
+            plugins: [new InlineCSSModulesNamesPlugin([/\.css$/])],
+        };
 
-		webpack(config, err => {
-			expect(err).toBeNull();
+        webpack(config, (err) => {
+            expect(err).toBeNull();
 
-			const outputFile = path.join(outputDirectory, 'main.js');
-			const outputFileContent = fs.readFileSync(outputFile, 'utf8');
-			expect(outputFileContent).toMatchSnapshot('Output bundle should match snapshot');
+            const outputFile = path.join(outputDirectory, 'main.js');
+            const outputFileContent = fs.readFileSync(outputFile, 'utf8');
+            expect(outputFileContent).toMatchSnapshot('Output bundle should match snapshot');
 
-			done();
-		});
-	});
+            done();
+        });
+    });
 
-	test('should produce expected output', done => {
-		const inputDirectory = path.join(fixturesDirectory, 'with-variable');
-		const outputDirectory = path.join(buildDirectory, 'with-variable');
-		const config = {
-			context: inputDirectory,
-			entry: './index.js',
-			mode: 'production',
-			target: 'node',
-			module: {
-				rules: [
-					{
-						test: /\.css/,
-						use: [{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								onlyLocals: true,
-							},
-						}]
-					}
-				]
-			},
-			optimization: {
-				runtimeChunk: false,
-				moduleIds: 'named',
-				chunkIds: 'named',
-				minimize: false,
-			},
-			output: {
-				path: outputDirectory,
-			},
-			plugins: [
-				new InlineCSSModulesNamesPlugin([/\.css$/]),
-			],
-		};
+    test('should produce expected output', (done) => {
+        const inputDirectory = path.join(fixturesDirectory, 'with-variable');
+        const outputDirectory = path.join(buildDirectory, 'with-variable');
+        const config = {
+            context: inputDirectory,
+            entry: './index.js',
+            mode: 'production',
+            target: 'node',
+            module: {
+                rules: [
+                    {
+                        test: /\.css/,
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: true,
+                                    onlyLocals: true,
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+            optimization: {
+                runtimeChunk: false,
+                moduleIds: 'named',
+                chunkIds: 'named',
+                minimize: false,
+            },
+            output: {
+                path: outputDirectory,
+            },
+            plugins: [new InlineCSSModulesNamesPlugin([/\.css$/])],
+        };
 
-		webpack(config, err => {
-			expect(err).toBeNull();
+        webpack(config, (err) => {
+            expect(err).toBeNull();
 
-			const outputFile = path.join(outputDirectory, 'main.js');
-			const outputFileContent = fs.readFileSync(outputFile, 'utf8');
-			expect(outputFileContent).toMatchSnapshot('Output bundle should match snapshot');
+            const outputFile = path.join(outputDirectory, 'main.js');
+            const outputFileContent = fs.readFileSync(outputFile, 'utf8');
+            expect(outputFileContent).toMatchSnapshot('Output bundle should match snapshot');
 
-			done();
-		});
-	});
-
+            done();
+        });
+    });
 });
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const HarmonyImportSideEffectDependency = require('webpack/lib/dependencies/HarmonyImportSideEffectDependency');
 const HarmonyImportSpecifierDependency = require('webpack/lib/dependencies/HarmonyImportSpecifierDependency');
 const ConstDependency = require('webpack/lib/dependencies/ConstDependency');
-const SourceLocation = require("acorn").SourceLocation; // TODO: Should be listed in peerDependency?
+const SourceLocation = require('acorn').SourceLocation; // TODO: Should be listed in peerDependency?
 
 function removeDependency(module, dep) {
     module.removeDependency(dep);
-    dep.module.reasons = dep.module.reasons.filter(r => r.dependency !== dep);
+    dep.module.reasons = dep.module.reasons.filter((r) => r.dependency !== dep);
 }
 
 class InlineCSSModuleNamesPlugin {
-	/*
-	 * `matchers` is a regular expression or an array of regular expressions that are used
-	 * to match module paths. Only constants from matching modules will be inlined.
-	 */
+    /*
+     * `matchers` is a regular expression or an array of regular expressions that are used
+     * to match module paths. Only constants from matching modules will be inlined.
+     */
     constructor(matchers) {
         if (Array.isArray(matchers)) {
             this.matchers = matchers;
@@ -22,110 +22,115 @@ class InlineCSSModuleNamesPlugin {
     }
 
     isConstantsModule(module) {
-        return module && this.matchers.some(matcher => matcher.test(module.resource));
+        return module && this.matchers.some((matcher) => matcher.test(module.resource));
     }
 
     apply(compiler) {
-        compiler.hooks.thisCompilation.tap(
-            'InlineCSSModuleNamesPlugin',
-            (compilation, { normalModuleFactory }) => {
-                compilation.hooks.optimizeDependencies.tap('InlineCSSModuleNamesPlugin', (modules) => {
-                    for (const module of modules) {
+        compiler.hooks.thisCompilation.tap('InlineCSSModuleNamesPlugin', (compilation, { normalModuleFactory }) => {
+            compilation.hooks.optimizeDependencies.tap('InlineCSSModuleNamesPlugin', (modules) => {
+                for (const module of modules) {
+                    const importSideEffectDependencies = new Map(); // Module -> Dependency
+                    const usesConstantDependencies = new Map(); // Module -> Boolean
+                    const usesNonConstantDependencies = new Map(); // Module -> Boolean
+                    const importSpecifierDependencies = []; // [ ImportSpecifierDependency ]
 
-                        const importSideEffectDependencies = new Map(); // Module -> Dependency
-                        const usesConstantDependencies = new Map(); // Module -> Boolean
-                        const usesNonConstantDependencies = new Map(); // Module -> Boolean
-                        const importSpecifierDependencies = []; // [ ImportSpecifierDependency ]
+                    for (const dep of module.dependencies) {
+                        if (!this.isConstantsModule(dep.module)) {
+                            continue;
+                        }
 
-                        for (const dep of module.dependencies) {
-                            if (!this.isConstantsModule(dep.module)) {
-                                continue;
+                        /*
+                         * The ImportSideEffectDependency is created whenever webpack sees an import statement:
+                         *   import { foo } from 'foo'
+                         * Webpack replaces the statement with code that loads the module and executes it
+                         *
+                         * We track these dependencies per-module because if all the imported bindings
+                         * turn out to be constants and we inline them, we'll remove the import completely.
+                         * That means that we treat constant modules as side-effect fee, i.e., we can safely
+                         * remove the import.
+                         */
+                        if (dep instanceof HarmonyImportSideEffectDependency) {
+                            importSideEffectDependencies.set(dep.module, dep);
+                        }
+
+                        if (dep instanceof HarmonyImportSpecifierDependency) {
+                            const classNames = JSON.parse(dep.module._source.source().match(/{.*}/s)[0]);
+
+                            let canRemove = true;
+
+                            let value;
+                            // Clone
+                            let loc = new SourceLocation(module.parser, dep.loc.start, dep.loc.end);
+                            let range = dep.range;
+
+                            // styles.Example - dot object access
+                            const simple = module._source
+                                .source()
+                                .slice(dep.range[1])
+                                .match(/^\.(\w+)/);
+                            if (simple) {
+                                value = classNames[simple[1]];
+                                loc.end = loc.end.offset(simple[0].length);
+                                range[1] += simple[0].length;
                             }
 
-                            /*
-                             * The ImportSideEffectDependency is created whenever webpack sees an import statement:
-                             *   import { foo } from 'foo'
-                             * Webpack replaces the statement with code that loads the module and executes it
-                             *
-                             * We track these dependencies per-module because if all the imported bindings
-                             * turn out to be constants and we inline them, we'll remove the import completely.
-                             * That means that we treat constant modules as side-effect fee, i.e., we can safely
-                             * remove the import.
-                             */
-                            if (dep instanceof HarmonyImportSideEffectDependency) {
-                                importSideEffectDependencies.set(dep.module, dep);
-                            }
-
-                            if (dep instanceof HarmonyImportSpecifierDependency) {
-                                const classNames = JSON.parse(dep.module._source.source().match(/{.*}/s)[0]);
-
-                                let canRemove = true;
-
-                                let value;
-                                // Clone
-                                let loc = new SourceLocation(module.parser, dep.loc.start, dep.loc.end);
-                                let range = dep.range;
-
-                                // styles.Example - dot object access
-                                const simple = module._source.source().slice(dep.range[1]).match(/^\.(\w+)/);
-                                if (simple) {
-                                    value = classNames[simple[1]];
-                                    loc.end = loc.end.offset(simple[0].length);
-                                    range[1] += simple[0].length;
-                                }
-
-                                const bracked = module._source.source().slice(dep.range[1]).match(/^\[(.+?)\]/);
-                                if (bracked) {
-                                    const key = bracked[1];
-                                    if (key[0] === "'" || key[0] === '"') {
-                                        // styles['Example'] or styles["Example"]
+                            const bracked = module._source
+                                .source()
+                                .slice(dep.range[1])
+                                .match(/^\[(.+?)\]/);
+                            if (bracked) {
+                                const key = bracked[1];
+                                if (key[0] === "'" || key[0] === '"') {
+                                    // styles['Example'] or styles["Example"]
+                                    value = classNames[bracked[1].substr(1, bracked[1].length - 2)];
+                                    loc.end = loc.end.offset(bracked[0].length);
+                                    range[1] += bracked[0].length;
+                                } else if (key[0] === '`') {
+                                    // styles[`Example`] - template string
+                                    // This case is problematic, because it can contains variables - in that case we cannot inline classname
+                                    if (/\${.+}/.test(key)) {
+                                        // We have variable in the template string - ignore
+                                        continue;
+                                    } else {
                                         value = classNames[bracked[1].substr(1, bracked[1].length - 2)];
                                         loc.end = loc.end.offset(bracked[0].length);
                                         range[1] += bracked[0].length;
-                                    } else if (key[0] === '`') {
-                                        // styles[`Example`] - template string
-                                        // This case is problematic, because it can contains variables - in that case we cannot inline classname
-                                        if (/\${.+}/.test(key)) {
-                                            // We have variable in the template string - ignore
-                                            continue
-                                        } else {
-                                            value = classNames[bracked[1].substr(1, bracked[1].length - 2)];
-                                            loc.end = loc.end.offset(bracked[0].length);
-                                            range[1] += bracked[0].length;
-                                        }
                                     }
                                 }
-
-                                const inlineDep = new ConstDependency(`/* inline-classname */ ${value ? '"' + value + '"' : value}`, dep.range);
-                                inlineDep.loc = loc;
-                                module.addDependency(inlineDep);
-
-                                // Mark the dependency for removal: we'll remove it after we're finished
-                                // traversing the dependency graph.
-                                importSpecifierDependencies.push(dep);
-
-                                /* Remember the fact that we inlined some constant. We remove import statements
-                                 * only for modules where we inlined at least something. The other modules we
-                                 * leave as they are. */
-                                usesConstantDependencies.set(dep.module, true);
                             }
-                        }
 
-                        /* Remove the import statements if all the imported bindings were inlined */
-                        for (const [depModule, depImport] of importSideEffectDependencies) {
-                            if (usesConstantDependencies.get(depModule)) {
-                                removeDependency(module, depImport);
-                            }
-                        }
+                            const inlineDep = new ConstDependency(
+                                `/* inline-classname */ ${value ? '"' + value + '"' : value}`,
+                                dep.range,
+                            );
+                            inlineDep.loc = loc;
+                            module.addDependency(inlineDep);
 
-                        /* Remove the ImportSpecifierDependencies that were replaced with ConstDependencies */
-                        for (const dep of importSpecifierDependencies) {
-                            removeDependency(module, dep);
+                            // Mark the dependency for removal: we'll remove it after we're finished
+                            // traversing the dependency graph.
+                            importSpecifierDependencies.push(dep);
+
+                            /* Remember the fact that we inlined some constant. We remove import statements
+                             * only for modules where we inlined at least something. The other modules we
+                             * leave as they are. */
+                            usesConstantDependencies.set(dep.module, true);
                         }
                     }
-                });
-            }
-        );
+
+                    /* Remove the import statements if all the imported bindings were inlined */
+                    for (const [depModule, depImport] of importSideEffectDependencies) {
+                        if (usesConstantDependencies.get(depModule)) {
+                            removeDependency(module, depImport);
+                        }
+                    }
+
+                    /* Remove the ImportSpecifierDependencies that were replaced with ConstDependencies */
+                    for (const dep of importSpecifierDependencies) {
+                        removeDependency(module, dep);
+                    }
+                }
+            });
+        });
     }
 }
 
